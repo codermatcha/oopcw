ba3d35590d97c23317dc7b3a6e1cc0ffdd29e229
Shin Thant Lwin <140297765+codermatcha@users.noreply.github.com>
2024-03-11 12:05:30 +0000

    Add files via upload

---
+++ draft.java
@@ -0,0 +1,17 @@
+package cycling;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import javax.naming.InvalidNameException;
+
+/**
+ * BadCyclingPortal is a minimally compiling, but non-functioning implementor
+ * of the CyclingPortal interface.
+ */
+
+ public class BadCyclingPortalImpl implements CyclingPortal {
+    
+
+ }
